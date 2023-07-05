d698e2272b93f2e1439a32ee89464d00b3f58498
Sushij <sushant.wurk@gmail.com>
2023-07-05 21:02:28 +0530

    Nav now shows correct tab that is opened

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,12 +3,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   @Output() componentSelected = new EventEmitter<string>();
+  loadedComp: string = "recipe";
+
 
   onSelect(component: string): void {
     this.componentSelected.emit(component);
+    this.loadedComp = component;
   }
 }
