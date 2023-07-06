ad58bb929ea021ca50d81bb4a3b892f69803fe2d
Sushij <sushant.wurk@gmail.com>
2023-07-06 21:12:34 +0530

    We actually cooking now though

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,4 +10,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent {
   // @ts-ignore
   @Input() recipe: Recipe;
+
+  constructor(private rlService: RecipeService) { }
+
+  onAddToList() {
+    this.rlService.addIngToShoppingList(this.recipe.ingredients)
+  }
 }
--- src/app/services/shoppingList.service.ts
+++ src/app/services/shoppingList.service.ts
@@ -4,16 +4,19 @@ import { Ingredient } from "../shared/ingredients.model";
 export class ShoppingListService {
   ingChanged = new EventEmitter<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [
-    new Ingredient("Test", 1),
-    new Ingredient("Test2", 2),
-  ]
+  private ingredients: Ingredient[] = []
 
   getIngedrients() {
     return this.ingredients.slice()
   }
   onIngAdded(ing: Ingredient) {
     this.ingredients.push(ing)
+    this.ingChanged.emit(this.ingredients.slice());
+  }
+  addIngs(ings: Ingredient[]) {
+    ings.forEach((ing) => {
+      this.ingredients.push(ing);
+    })
     this.ingChanged.emit(this.ingredients.slice());
   }
 }
